843ca8409bbd0fdde89726dd2af66b066d4c1ae6
Ana Clara Mansano <anamansano@jera.com.br>
2022-08-05 22:58:41 -0300

    [UPDATE] music data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Song.create([
+              {
+                name: 'Beauty and a Beat',
+                album: 'Believe',
+                artist: 'Justin Bieber'
+              },
+              {
+                name: 'This Love',
+                album: 'Romance',
+                artist: 'Camila Cabello'
+              },
+              {
+                name: 'Stay',
+                album: 'Be',
+                artist: 'BTS'
+              },
+              {
+                name: 'Because I Had You',
+                album: 'Shawn Mendes The Album',
+                artist: 'Shawn Mendes'
+              },
+              {
+                name: 'brutal',
+                album: 'SOUR',
+                artist: 'Olivia Rodrigo'
+              },
+              {
+                name: 'Free Time',
+                album: 'Free Time',
+                artist: 'Ruel'
+              },
+              {
+                name: 'Cornelia Street',
+                album: 'Lover',
+                artist: 'Taylor Swift'
+              }
+            ])
